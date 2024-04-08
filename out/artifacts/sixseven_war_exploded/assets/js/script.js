5eb62d8a2f71ba9f1eb22eb56689fbed0751d384
小呆 <122683540+shuaidaia@users.noreply.github.com>
2024-04-08 12:24:33 +0800

    Delete out directory
    删除out目录

--- out/artifacts/sixseven_war_exploded/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-$(function(){
-	$(".isdel").click(function(){
-		return confirm("真的要删除吗？");
-		});
-	$(".tblist th:odd").addClass("even");
-	$(".tblist tr:odd").addClass("odd");
-	
-	});
